666ed8e31e657fbdf3fb76f8b5527341bcb3b9ea
Aaron Barnes <aaron@io.nz>
2021-10-01 20:29:43 +1300

    Add AST tree validation to parser tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,27 +1,84 @@
 import pytest
 
 from jql.parser import jql_parser, JqlTransformer
+from jql.types import Content, Flag, Ref, Tag, Value
 
 
 examples = [
-    "CREATE hi",
-    "CREATE go to supermarket #todo #todo/completed",
-    "CREATE do dishes #todo #chores",
-    "CREATE book appointment #todo #todo/remind_at=20210412",
-    "@d2a SET #todo/completed",
-    "@3dd SET book appointment at physio",
-    "@544 SET #book #todo",
-    "@aaa",
-    "@f4a HISTORY",
-    "#todo/completed",
-    "do dishes",
-    "find #todo",
-    "#todo/remind_at=444"
+    [
+        "CREATE hi",
+        "create",
+        [Content("hi")]
+    ],
+    [
+        "CREATE go to supermarket #todo #todo/completed",
+        "create",
+        [Content("go to supermarket"), Tag("todo"), Flag("todo", "completed")]
+    ],
+    [
+        "CREATE do dishes #todo #chores",
+        "create",
+        [Content("do dishes"), Tag("todo"), Tag("chores")]
+    ],
+    [
+        "CREATE book appointment #todo #todo/remind_at=20210412",
+        "create",
+        [Content("book appointment"), Tag("todo"), Value("todo", "remind_at", "20210412")]
+    ],
+    [
+        "@d2a SET #todo/completed",
+        "set",
+        [Ref("d2a"), Flag("todo", "completed")]
+    ],
+    [
+        "@3dd SET book appointment at physio",
+        "set",
+        [Ref("3dd"), Content("book appointment at physio")]
+    ],
+    [
+        "@544 SET #book #todo",
+        "set",
+        [Ref("544"), Tag("book"), Tag("todo")]
+    ],
+    [
+        "@aaa",
+        "get",
+        [Ref("aaa")]
+    ],
+    [
+        "@f4a HISTORY",
+        "history",
+        [Ref("f4a")]
+    ],
+    [
+        "#todo/completed",
+        "list",
+        [Flag("todo", "completed")]
+    ],
+    [
+        "do dishes",
+        "list",
+        [Content("do dishes")]
+    ],
+    [
+        "find #todo",
+        "list",
+        [Content("find"), Tag("todo")]
+    ],
+    [
+        "#todo/remind_at=444",
+        "list",
+        [Value("todo", "remind_at", "444")]
+    ]
 ]
 
 
-@pytest.mark.parametrize("test_query", examples)
-def test_parser(test_query: str) -> None:
-    tree = jql_parser.parse(test_query)
-    # print(tree)
-    JqlTransformer().transform(tree)
+@pytest.mark.parametrize("test", examples)
+def test_parser(test) -> None:
+    query, action, result = test
+
+    tree = jql_parser.parse(query)
+    ast = JqlTransformer().transform(tree)
+
+    assert ast.data == action
+    assert ast.children == result
