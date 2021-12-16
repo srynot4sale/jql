2c2f06fec0f359a7d5bb6784c255c22ee82f17d6
Aaron Barnes <aaron@io.nz>
2021-12-16 14:14:03 +1300

    webjql: Make web UI multi database aware

--- webjql/__init__.py
+++ webjql/__init__.py
@@ -1,30 +1,13 @@
-from flask import Flask, request, render_template, g, redirect
+from flask import abort, Flask, request, render_template, g, redirect
 from itertools import filterfalse
-import logging
 from typing import Any, Dict
-import urllib
 
 
-from jql.client import Client
-from jql.sqlite import SqliteStore
-from jql.types import is_flag, is_tag, is_primary_ref, get_props, get_tags, get_flags, get_value, has_ref, has_sys_tag, Ref, single, Tag, Flag, Fact, tag_eq
+import webjql.lib as lib
+from jql.types import is_tag, is_primary_ref, get_props, get_tags, get_flags, get_value, has_ref, has_sys_tag, Ref, single, Tag, Flag, Fact, tag_eq
 
 
 app = Flask(__name__)
-
-DATABASE = 'web.jdb'
-
-
-def url_to_query(url: str) -> str:
-    query = url
-    query = query.replace('~', '#')
-    return query
-
-
-def query_to_url(query: str) -> str:
-    url = query
-    url = url.replace('#', '~')
-    return url
 
 
 @app.context_processor
@@ -36,59 +19,20 @@ def jql_utilities():  # type: ignore
 def html_utilities() -> Dict[str, Any]:
     def make_link(fact: Fact) -> str:
         if is_primary_ref(fact):
-            link = f'/{fact.value}'
+            link = f'{fact.value}'
         else:
             strfact = str(fact)
-            url = query_to_url(strfact)
-            link = f'/q/{url}'
+            url = lib.query_to_url(strfact)
+            link = f'q/{url}'
 
-        return f'<a href="{link}">{fact}</a>'
+        return f'<a href="/{g.database}/{link}">{fact}</a>'
 
     return dict(make_link=make_link)
 
 
-def get_toc():
-    tx = get_client().new_transaction()  # type: ignore
-    all_tags = tx.q("HINTS")
-    full_count = None
-    primary_tag = None
-
-    # Find the full count by looking for #db tag
-    for t in all_tags:
-        if not get_tags(t):
-            full_count = get_value(t, "db", "count")
-            break
-
-    if not full_count:
-        raise Exception("No items found")
-
-    # Look for any tags that everything has
-    tags = []
-    for t in all_tags:
-        if not get_tags(t):
-            continue
-
-        if get_value(t, "db", "count") == full_count:
-            if primary_tag:
-                raise Exception("Multiple primary tags found")
-            primary_tag = single(get_tags(t))
-        else:
-            tags.append((single(get_tags(t)), get_value(t, "db", "count")))
-
-    return dict(tags=tags, primary_tag=primary_tag, full_count=full_count)
-
-
 @app.context_processor
 def app_utilities():  # type: ignore
-    return get_toc()
-
-
-def get_client():  # type: ignore
-    client = getattr(g, '_client', None)
-    if client is None:
-        store = SqliteStore(location=DATABASE)
-        client = Client(store=store, client="web:user", log_level=logging.ERROR)
-    return client
+    return lib.get_toc()
 
 
 @app.teardown_appcontext
@@ -98,24 +42,36 @@ def close_connection(exception):  # type: ignore
         del client
 
 
-@app.route("/query")
-def results():  # type: ignore
+@app.route("/favicon.ico")
+def notfound():  # type: ignore
+    abort(404)
+
+
+@app.route("/")
+def list_dbs():  # type: ignore
+    return render_template('dbs.html', databases=lib.get_databases())
+
+
+@app.route("/<db>/query")
+def results(db):  # type: ignore
+    g.database = db
     query = request.args.get('q', '')
 
     if not len(query):
-        return redirect('/')
+        return redirect(f'/{db}/')
 
     if query.startswith('@') and ' ' not in query:
-        return redirect(f'/{query.lstrip("@")}')
+        return redirect(f'/{db}/{query.lstrip("@")}')
 
-    return redirect(f'/q/{query_to_url(query)}')
+    return redirect(f'/{db}/q/{lib.query_to_url(query)}')
 
 
-@app.route("/", defaults={"query": ""})
-@app.route("/q/<path:query>")
-def query(query):  # type: ignore
+@app.route("/<db>/", defaults={"query": ""})
+@app.route("/<db>/q/<path:query>")
+def query(db, query):  # type: ignore
+    g.database = db
 
-    query = url_to_query(query)
+    query = lib.url_to_query(query)
 
     # Try figure out the context
     context = []
@@ -123,7 +79,7 @@ def query(query):  # type: ignore
 
     tag = None
     if not len(query):
-        tag = get_toc()['primary_tag'].tag
+        tag = lib.get_toc()['primary_tag'].tag
         query = f'#{tag}'
     else:
         if query.startswith('#') and ' ' not in query:
@@ -137,18 +93,20 @@ def query(query):  # type: ignore
                 tag = q
 
     if tag:
-        tx = get_client().new_transaction()  # type: ignore
+        tx = lib.get_client().new_transaction()
         props = [(single(filterfalse(has_sys_tag, get_flags(t))), get_value(t, "db", "count")) for t in tx.q(f"HINTS #{tag}/") if get_flags(t)]
 
-    tx = get_client().new_transaction()  # type: ignore
+    tx = lib.get_client().new_transaction()
     items = tx.q(query)
 
     return render_template('tag.html', title=query, context=context, props=props, items=items)
 
 
-@app.route("/<ref>")
-def ref(ref):  # type: ignore
-    tx = get_client().new_transaction()  # type: ignore
+@app.route("/<db>/<ref>")
+def ref(db, ref):  # type: ignore
+    g.database = db
+
+    tx = lib.get_client().new_transaction()
     item = tx.q(f"@{ref}")[0]
     item_tags = [Tag('db')] + [t for t in get_tags(item) if next(filter(tag_eq(t.tag), get_props(item)), None) is not None]
 
---
+++ webjql/lib.py
@@ -0,0 +1,107 @@
+import logging
+import os.path
+from typing import Dict, List
+from flask import g
+
+
+from jql.client import Client
+from jql.sqlite import SqliteStore
+from jql.types import get_tags, get_value, single
+
+
+ROOTDIR = os.path.join(os.getcwd(), 'dbs')
+DATABASES = {}
+
+
+def get_databases() -> List[str]:
+    global DATABASES
+
+    dbs = []
+
+    for e in os.listdir(ROOTDIR):
+        if e.endswith('.jdb'):
+            dbs.append(e[:-4])
+
+    for d in DATABASES.keys():
+        if d not in dbs:
+            del DATABASES[d]
+    
+    for d in dbs:
+        if d not in DATABASES.keys():
+            print(f'Loading new store {d}')
+            DATABASES[d] = SqliteStore(location=db_path(d))
+
+    return dbs
+
+
+def url_to_query(url: str) -> str:
+    query = url
+    query = query.replace('~', '#')
+    return query
+
+
+def query_to_url(query: str) -> str:
+    url = query
+    url = url.replace('#', '~')
+    return url
+
+
+def db_path(database: str) -> str:
+    path = os.path.join(ROOTDIR, f'{database}.jdb')
+    if not os.path.isfile(path):
+        raise Exception(f'{database} does not exist ({path})')
+    return path
+
+
+def get_client() -> Client:
+    database = g.database
+
+    # Confirm database str is a-z only
+    if not database.isalpha():
+        raise Exception(f'{database} is invalid')
+
+    # If not in DATABASES, rescan
+    if database not in DATABASES:
+        dbs = get_databases()
+        if database not in dbs:
+            raise Exception(f'{database} does not exist!')
+
+    if not hasattr(g, '_client'):
+        print('New client created')
+        g._client = Client(store=DATABASES[database], client="web:user", log_level=logging.ERROR)
+
+    return g._client
+
+
+def get_toc():  # type: ignore
+    if not hasattr(g, 'database'):
+        return {}
+
+    tx = get_client().new_transaction()
+    all_tags = tx.q("HINTS")
+    full_count = None
+    primary_tag = None
+
+    # Find the full count by looking for #db tag
+    for t in all_tags:
+        if not get_tags(t):
+            full_count = get_value(t, "db", "count")
+            break
+
+    if not full_count:
+        raise Exception("No items found")
+
+    # Look for any tags that everything has
+    tags = []
+    for t in all_tags:
+        if not get_tags(t):
+            continue
+
+        if get_value(t, "db", "count") == full_count:
+            if primary_tag:
+                raise Exception("Multiple primary tags found")
+            primary_tag = single(get_tags(t))
+        else:
+            tags.append((single(get_tags(t)), get_value(t, "db", "count")))
+
+    return dict(tags=tags, primary_tag=primary_tag, full_count=full_count)
